7cf9c2210823f7d7e080e5d5044251eb0196c2bc
Pelegrinetti <pelegrinetti.dev@gmail.com>
2022-04-27 19:12:39 -0300

    Use public runtime config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
+    techStoreFrontendApi: process.env.TECH_STORE_FRONTEND_API,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
