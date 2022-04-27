a04c84fd4f3a474283bacb481ed397bdaba92e81
Pelegrinetti <pelegrinetti.dev@gmail.com>
2022-04-27 19:18:57 -0300

    Add fake products and sections

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,8 +1,62 @@
+import { Flex } from '@chakra-ui/react';
 import SiteHeader from '../../shared/SiteHeader';
+import ProductCard from '../../shared/ProductCard';
+import Section from '../../shared/Section';
 
 function Home() {
+  const product = {
+    title: 'PC Top',
+    image: {
+      url: 'https://via.placeholder.com/300x300/09f.png/fff',
+      description: 'Lets bora',
+    },
+    price: 3000,
+    rating: 5,
+    url: 'http://localhost',
+  };
+
   return (
-    <SiteHeader />
+    <>
+      <SiteHeader />
+      <Flex
+        maxWidth={{
+          lg: 'container.xl',
+        }}
+        margin="auto"
+        flexDirection="column"
+      >
+        <Section title="Os melhores produtos">
+          {[...Array(8)].map((item, i) => (
+            <ProductCard
+              key={i}
+              title={product.title}
+              image={product.image}
+              price={product.price}
+            />
+          ))}
+        </Section>
+        <Section title="Os melhores produtos">
+          {[...Array(8)].map((item, i) => (
+            <ProductCard
+              key={i}
+              title={product.title}
+              image={product.image}
+              price={product.price}
+            />
+          ))}
+        </Section>
+        <Section title="Os melhores produtos">
+          {[...Array(8)].map((item, i) => (
+            <ProductCard
+              key={i}
+              title={product.title}
+              image={product.image}
+              price={product.price}
+            />
+          ))}
+        </Section>
+      </Flex>
+    </>
   );
 }
 
