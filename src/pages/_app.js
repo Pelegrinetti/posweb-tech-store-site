75789e1fb406c62f782c54b01299263a7ca2a671
Pelegrinetti <pelegrinetti.dev@gmail.com>
2022-05-05 19:33:18 -0300

    Use auth provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import Script from 'next/script';
-import getConfig from 'next/config';
+import { SessionProvider } from 'next-auth/react';
 
-function CustomApp({ Component, pageProps }) {
-  const { googleClientId, techStoreFrontendApi } = getConfig().publicRuntimeConfig;
-
+function CustomApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
+    <SessionProvider session={session}>
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
-      <Script
-        src="https://accounts.google.com/gsi/client"
-        strategy="beforeInteractive"
-      />
-      <div
-        id="g_id_onload"
-        data-client_id={googleClientId}
-        data-login_uri={`${techStoreFrontendApi}/login`}
-      />
-    </>
+    </SessionProvider>
   );
 }
 
