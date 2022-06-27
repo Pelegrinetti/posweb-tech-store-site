8fcd4140288bb815819420eabcd943e253eba2a8
Pelegrinetti <pelegrinetti.dev@gmail.com>
2022-06-27 13:04:49 -0300

    Add cart item

---
+++ src/components/shared/CartItem/CartItem.js
@@ -0,0 +1,77 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Image,
+  ListItem,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Text,
+} from '@chakra-ui/react';
+
+function CartItem(props) {
+  const { item } = props;
+  const [image] = item.gallery;
+
+  return (
+    <ListItem
+      padding={2}
+      borderWidth={1}
+      borderRadius="5px"
+      listStyleType="none"
+      gap={2}
+    >
+      <Flex>
+        <Image
+          src={image.url}
+          alt={image.description}
+          width="100px"
+          height="100px"
+          objectFit="cover"
+          objectPosition="center"
+        />
+        <Box>
+          <Heading as="h4" size="md">
+            {item.name}
+          </Heading>
+          <Text noOfLines={2}>{item.description}</Text>
+          <Box>
+            {item.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+            <Box as="span" color="gray.600" fontSize="sm">
+              {' '}
+              / unidade
+            </Box>
+          </Box>
+        </Box>
+      </Flex>
+      <Divider />
+      <Box padding={2}>
+        <FormControl alignItems="baseline">
+          <FormLabel htmlFor="quantity">Quantidade:</FormLabel>
+          <NumberInput id="quantity" defaultValue={item.quantity} min={1} marginBottom={2}>
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+          <Box>
+            <Button colorScheme="red">Remover</Button>
+          </Box>
+        </FormControl>
+      </Box>
+    </ListItem>
+  );
+}
+
+export default CartItem;
